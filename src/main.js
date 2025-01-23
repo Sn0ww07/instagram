1bc02032e05e09c9a1b9be357f84d51352e80ae5
Nowt <snowquiet07@gmail.com>
2025-01-23 16:20:38 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
+document.addEventListener('DOMContentLoaded', (event) => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
 
-    // Exibir a localização na página
-    document.getElementById('latitude').textContent = `Latitude: ${latitude}`;
-    document.getElementById('longitude').textContent = `Longitude: ${longitude}`;
-  });
-} else {
-  console.log("Geolocalização não é suportada por este navegador.");
-}
+      // Exibir a localização na página
+      document.getElementById('latitude').textContent = `Latitude: ${latitude}`;
+      document.getElementById('longitude').textContent = `Longitude: ${longitude}`;
+    }, function(error) {
+      console.error('Erro ao obter localização:', error);
+    });
+  } else {
+    console.log("Geolocalização não é suportada por este navegador.");
+  }
+});
