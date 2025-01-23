810ae0f563b485c9aa92153be97078e7485ba3cc
Nowt <snowquiet07@gmail.com>
2025-01-23 16:19:46 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,29 +3,10 @@ if (navigator.geolocation) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
 
-    // Enviar a localização para o servidor
-    enviarLocalizacao(latitude, longitude);
+    // Exibir a localização na página
+    document.getElementById('latitude').textContent = `Latitude: ${latitude}`;
+    document.getElementById('longitude').textContent = `Longitude: ${longitude}`;
   });
 } else {
   console.log("Geolocalização não é suportada por este navegador.");
-}
-
-function enviarLocalizacao(latitude, longitude) {
-  fetch('http://localhost:3000/api/localizacao', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      latitude: latitude,
-      longitude: longitude
-    })
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log('Localização enviada com sucesso:', data);
-  })
-  .catch((error) => {
-    console.error('Erro ao enviar localização:', error);
-  });
 }
